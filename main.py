4c03f41327b952428ff333e807398e15703d8937
MurMurich <149789899+MurMurichOG@users.noreply.github.com>
2024-05-05 20:51:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import asyncio
 from aiogram import types
 import logging
 from handlers.start import start_router
-from hw4.bot import bot, dp
-from hw4.shop import shop_router
-from handlers.table import init, create, populate
+from bot import bot, dp
+from games import games_router
 
 
 async def on_startup():
     print('Online!')
-    init()
-    create()
-    populate()
-
-
 async def main():
     await bot.set_my_commands([
         types.BotCommand(command="start", description="Начало")
     ])
     dp.include_router(start_router)
-    dp.include_router(shop_router)
+    dp.include_router(games_router)
     dp.startup.register(on_startup)
+
     await dp.start_polling(bot)
 
 
